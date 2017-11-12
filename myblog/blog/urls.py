31cae65a12772c4b3ea3076bcc9201699c4df1e1
chenliang <18019326827@163.com>
2017-11-12 22:51:56 +0800

    c
    c

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^article/edit/(?P<article_id>[0-9]+)$', views.edit_article, name='edit_article'),
     url(r'^article/update/$', views.save_edit_article, name='update_article'),
     url(r'^article/new$', views.new_article, name="new_article"),
-    url(r'^article/save/$', views.save_new_article, name="create_article"),
+    url(r'^article/save$', views.save_new_article, name="create_article"),
     url(r'^article/delete/(?P<article_id>[0-9]+)$', views.delete_article, name='delete_article')
 ]
